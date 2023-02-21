0f0d61c679881541f9a8aa6e43bedde7b05c1662
emarti2 <eva.martin@bsc.es>
2023-02-21 14:15:48 +0100

    added logging when exiting due to error

--- main.py
+++ main.py
@@ -40,72 +40,81 @@ def get_bioconda_biotools_galaxy_tools(tool):
 
 
 def import_data():
-    # 0.1 Set up logging
-    parser = argparse.ArgumentParser(
-        description="Importer of OpenEBench tools from OpenEBench Tool API"
-    )
-    parser.add_argument(
-        "--loglevel", "-l",
-        help=("Set the logging level"),
-        default="INFO",
-    )
-    parser.add_argument(
-        "--logdir", "-d",
-        help=("Set the logging directory"),
-        default="./logs/summary.log",
-    )
-    args = parser.parse_args()
-    numeric_level = getattr(logging, args.loglevel.upper())
-    logs_dir = args.logdir
+    try:
+        # 0.1 Set up logging
+        parser = argparse.ArgumentParser(
+            description="Importer of OpenEBench tools from OpenEBench Tool API"
+        )
+        parser.add_argument(
+            "--loglevel", "-l",
+            help=("Set the logging level"),
+            default="INFO",
+        )
+        parser.add_argument(
+            "--logdir", "-d",
+            help=("Set the logging directory"),
+            default="./logs/summary.log",
+        )
+        args = parser.parse_args()
+        numeric_level = getattr(logging, args.loglevel.upper())
+        logs_dir = args.logdir
 
-    logging.basicConfig(level=numeric_level, format='%(asctime)s - %(levelname)s - opeb_tools - %(message)s', filename=f'{logs_dir}', filemode='w')
+        logging.basicConfig(level=numeric_level, format='%(asctime)s - %(levelname)s - opeb_tools - %(message)s', filename=f'{logs_dir}', filemode='w')
 
-    # 0.2 Load .env
-    load_dotenv()
-    logging.info("state_importation - 1")
-
-
-    # 1. connect database/set output file
-    logging.info('Connecting to database')
-    STORAGE_MODE = os.getenv('STORAGE_MODE', 'db')
-
-    if STORAGE_MODE =='db':
-        alambique = connect_db()
-
-    else:
-        OUTPUT_PATH = os.getenv('OUTPUT_PATH', './data/opebtools.json')
+        # 0.2 Load .env
+        load_dotenv()
+        logging.info("state_importation - 1")
 
 
-    # 2. Download all opeb
-    logging.info('Downloading OPEB tools')
-    URL_OPEB_TOOLS = os.getenv('URL_OPEB_TOOLS', 'https://openebench.bsc.es/monitor/tool')
-    logging.info(f'OpenEBench tools URL: {URL_OPEB_TOOLS}')
-    
-    tools = get_url(URL_OPEB_TOOLS)
-    if tools:
-    
-        logging.info('Tools obtained')
-        # 3. Get tools
-        logging.info(f'Processing {len(tools)} tools ...')
-        #For tool in OPEB Tool db
-        for tool in tools:       
-            # 4. Process metadata
-            tool = get_bioconda_biotools_galaxy_tools(tool)
+        # 1. connect database/set output file
+        logging.info('Connecting to database')
+        STORAGE_MODE = os.getenv('STORAGE_MODE', 'db')
 
-            # 5. push to db/file
-            if STORAGE_MODE=='db':
-                push_entry(tool, alambique)
+        if STORAGE_MODE =='db':
+            alambique = connect_db()
 
-            else:
-                save_entry(tool, OUTPUT_PATH)
+        else:
+            OUTPUT_PATH = os.getenv('OUTPUT_PATH', './data/opebtools.json')
+
+
+        # 2. Download all opeb
+        logging.info('Downloading OPEB tools')
+        URL_OPEB_TOOLS = os.getenv('URL_OPEB_TOOLS', 'https://openebench.bsc.es/monitor/tool')
+        logging.info(f'OpenEBench tools URL: {URL_OPEB_TOOLS}')
         
-        logging.info("state_importation - 0")
+        tools = get_url(URL_OPEB_TOOLS)
+        if tools:
+        
+            logging.info('Tools obtained')
+            # 3. Get tools
+            logging.info(f'Processing {len(tools)} tools ...')
+            #For tool in OPEB Tool db
+            for tool in tools:       
+                # 4. Process metadata
+                tool = get_bioconda_biotools_galaxy_tools(tool)
+
+                # 5. push to db/file
+                if STORAGE_MODE=='db':
+                    push_entry(tool, alambique)
+
+                else:
+                    save_entry(tool, OUTPUT_PATH)
+            
+        else:
+            logging.error('error - crucial_object_empty')
+            logging.error('No content to processed. content_decoded is empty. Exiting...')
+            logging.info("state_importation - 2")
+            exit(1) 
+        
+    except Exception as e:
+        logging.error(f'error - {type(e).__name__}')
+        logging.info("state_importation - 2")
+        exit(1)
 
     else:
-        logging.error('error - crucial_object_empty')
-        logging.error('No content to process. Exiting...')
         logging.info("state_importation - 0")
-        exit(1)
+    
+
 
 
 if __name__ == '__main__':
