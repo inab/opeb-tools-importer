e2b685b10889a328a0d038d4fca92f5306a20736
Eva Martin del Pico <eva.mdpico@gmail.com>
2024-02-28 17:52:05 +0100

    ci: requeriments update

--- main.py
+++ main.py
@@ -1,19 +1,10 @@
 import argparse
-import json
-from matplotlib.font_manager import findfont
-import requests
 import os
 import sys
 import logging
 
 from utils import get_url, connect_db, push_entry, add_metadata_to_entry
 
-
-session = requests.Session()
-headers = {
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit 537.36 (KHTML, like Gecko) Chrome",
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-}
 
 def get_source(id_):
     string = id_.split('/')[5]
